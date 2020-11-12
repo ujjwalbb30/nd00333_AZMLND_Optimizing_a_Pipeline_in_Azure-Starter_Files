2848274d4974c184f2ad02f7c5056c7fa9f591d8
ujjwalbb30 <58893950+ujjwalbb30@users.noreply.github.com>
2020-11-12 22:18:12 +0530

    README, train.py  & udacity-project.ipynb
    Completed README.md and answered all the questions. Completed training script train.py. Completed udacity-project.ipynb python notebook.

--- train.py
+++ train.py
@@ -14,16 +14,6 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv",validate=True, include_path=False, infer_column_types=True, set_column_types=None, separator=',', header=True, partition_format=None, support_multi_line=False, empty_as_string=False, encoding='utf8')
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33,random_state=42)
-
-run = Run.get_context()
-
 def clean_data(data):
     # Dict for cleaning data
     months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
@@ -49,6 +39,7 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
     
 
 def main():
@@ -67,6 +58,11 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-
+    
 if __name__ == '__main__':
+    ds = TabularDatasetFactory.from_delimited_files("https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
+    x, y = clean_data(ds)
+    # TODO: Split data into train and test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.33,random_state=42)
+    run = Run.get_context()
     main()
